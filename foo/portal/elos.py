043f60fa5d887d0ed70b6bef7728a483021cc398
zhouchao123 <zc2473@163.com>
2017-04-10 09:58:34 +0800

    ...

--- foo/portal/elos.py
+++ foo/portal/elos.py
@@ -899,6 +899,79 @@ class ElosBlogCreatHandler(AuthorizationHandler):
         self.redirect("/elos/clubs/"+ CLUB_ID +"/articles/draft")
 
 
+class ElosBlogDetailHandler(AuthorizationHandler):
+    @tornado.web.authenticated  # if no session, redirect to login page
+    def get(self, club_id, article_id):
+        logging.info(self.request)
+
+        url = "http://api.7x24hs.com/api/clubs/"+club_id
+        http_client = HTTPClient()
+        response = http_client.fetch(url, method="GET")
+        logging.info("got response %r", response.body)
+        rs = json_decode(response.body)
+        club = rs['rs']
+        league_id = club['league_id']
+
+        url = "http://api.7x24hs.com/api/leagues/"+league_id+"/categories"
+        http_client = HTTPClient()
+        response = http_client.fetch(url, method="GET")
+        logging.info("got response %r", response.body)
+        rs = json_decode(response.body)
+        categories = rs['rs']
+
+        url = "http://api.7x24hs.com/api/articles/"+article_id
+        http_client = HTTPClient()
+        response = http_client.fetch(url, method="GET")
+        logging.info("got response %r", response.body)
+        rs = json_decode(response.body)
+        article_info = rs['rs']
+
+        html = article_info['paragraphs']
+        # 为图片延迟加载准备数据
+        # <img alt="" src="http://bighorn.b0.upaiyun.com/blog/2016/11/2/758f7478-d406-4f2e-9566-306a963fb979" />
+        # <img data-original="真实图片" src="占位符图片">
+        ptn="(<img src=\"http[s]*://[\w\.\/\-]+\" />)"
+        img_ptn = re.compile(ptn)
+        imgs = img_ptn.findall(html)
+        for img in imgs:
+            logging.info("got img %r", img)
+            ptn="<img src=\"(http[s]*://[\w\.\/\-]+)\" />"
+            url_ptn = re.compile(ptn)
+            urls = url_ptn.findall(html)
+            url = urls[0]
+            logging.info("got url %r", url)
+            #html = html.replace(img, "<img class=\"lazy\" data-original=\""+url+"\" src=\"/static/images/weui.png\" width=\"100%\" height=\"480\" />")
+            html = html.replace(img, "<img width='100%' src='"+url+"' />")
+        logging.info("got html %r", html)
+        article_info['paragraphs'] = html
+        article_info['publish_time'] = timestamp_friendly_date(article_info['publish_time'])
+
+        # popular(流行)
+        params = {"filter":"club", "club_id":club_id, "status":"publish", "category":"3801d62cf73411e69a3c00163e023e51", "idx":0, "limit":3}
+        url = url_concat("http://api.7x24hs.com/api/articles", params)
+        http_client = HTTPClient()
+        response = http_client.fetch(url, method="GET")
+        logging.info("got response %r", response.body)
+        rs = json_decode(response.body)
+        populars = rs['rs']
+        for article in populars:
+            article['publish_time'] = timestamp_friendly_date(article['publish_time'])
+
+        is_login = False
+        access_token = self.get_secure_cookie("access_token")
+        logging.info("got access_token>>>>> %r",access_token)
+        if access_token:
+            is_login = True
+
+        self.render('elos/blog-detail.html',
+                club=club,
+                is_login=is_login,
+                access_token=access_token,
+                categories=categories,
+                article=article_info,
+                populars=populars)
+
+
 class ElosBlogEditHandler(AuthorizationHandler):
     @tornado.web.authenticated  # if no session, redirect to login page
     def get(self, club_id, article_id):
@@ -966,6 +1039,7 @@ class ElosBlogEditHandler(AuthorizationHandler):
         self.render('elos/blog-edit.html',
                 club=club,
                 is_login=is_login,
+                access_token=access_token,
                 categories=categories,
                 article=article_info,
                 populars=populars)
@@ -1021,6 +1095,7 @@ class ElosBlogDraftHandler(AuthorizationHandler):
 
         self.render('elos/blog-draft.html',
                 is_login=is_login,
+                access_token=access_token,
                 club=club,
                 categories=categories,
                 articles=articles,
@@ -1077,6 +1152,7 @@ class ElosBlogPublishHandler(AuthorizationHandler):
 
         self.render('elos/blog-publish.html',
                 is_login=is_login,
+                access_token=access_token,
                 club=club,
                 categories=categories,
                 articles=articles,
@@ -1133,6 +1209,7 @@ class ElosBlogUserInfoHandler(AuthorizationHandler):
 
         self.render('elos/user-info.html',
                 is_login=is_login,
+                access_token=access_token,
                 club=club,
                 categories=categories,
                 articles=articles,
