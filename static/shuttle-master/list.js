601e5fa1ebc4119dee4b5eb9dc2a4bad1a11d800
zhouchao123 <zc2473@163.com>
2017-07-06 18:57:08 +0800

    样式表

---
+++ static/shuttle-master/list.js
@@ -0,0 +1,13 @@
+// 分销商列表 @2017/05/23
+// $(function(){
+//   console.log('to fetch distributor list...');
+//
+//   $('.todo-element').click(function(e){
+//     console.log('todo element clicked!');
+//
+//     console.log($(this));
+//     console.log($(this).data('id'));
+//     location.href = '/bf/wx/vendors/{{ club['club_id'] }}/supplier?s=' + $(this).data('id');
+//   });
+//
+// });
